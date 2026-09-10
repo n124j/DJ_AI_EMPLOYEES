58482932586d475a5c8310820b3358aa6dfd1881
Nick B <imlearningai25@gmail.com>
2026-09-09 22:05:00 -0400

    created support app and tools

---
+++ support/tools.py
@@ -0,0 +1,59 @@
+from orders.models import Order,RefundRequest
+from django.utils import timezone
+from .tracking_data import DELIVERY_DATA
+
+def get_order_details(order_id):
+
+    try:
+        order=Order.objects.get(id=order_id)
+        return {
+            "order_id":order.id,
+            "product_name": order.product_name,
+            "amount": str(order.amount),
+            "status":order.status,
+            "carrier":order.carrier,
+            "tracking_number":order.tracking_number,
+            "delivery_address":order.devivery,
+            "ordered_on":order.created_at.strftime("%d %b %Y"), # 25 May 2026
+            "days_since_order":(timezone.now() - order.created_at).days, # 20
+
+        }
+    except Order.DoesNotExist:
+        return {
+            "error": f"Order #{order_id} not found."
+        }
+
+def get_refund_history(user_id):
+
+
+    refunds=RefundRequest.objects.filter(user_id=user_id).order_by("-created_at") # hyphen will give the result in desc order
+
+    history=[]
+    for refund in refunds:
+        history.append({
+            "order_id":refund.order.id,
+            "product":refund.order.product_name,
+            "reason":refund.reason,
+            "status":refund.status,
+            "requested_on":refund.created_at.strftime("%d %b %Y"), # 25 May 2026
+        })
+
+    return {
+        "total_refun_requests":len(history),
+        "history":history,
+
+    }
+
+def check_delivery_status(tracking_number,carrier):
+    default_response={
+        "status":"Unknown",
+        "last_location":"Tracking info unavailable",
+        "last_update":"N/A",
+        "estimated_delivery":"Contact carrier directly",
+        "delay_reason":"No updates from carrier"
+    }
+    result=DELIVERY_DATA.get(tracking_number,default_response)
+
+    result["tracking_number"]=tracking_number
+    result["carrier"]=carrier
+    return result
