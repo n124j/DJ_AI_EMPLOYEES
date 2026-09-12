6f52c4c500338f4bd78b4ff1987f00854e1c88af
Nick B <imlearningai25@gmail.com>
2026-09-11 22:20:13 -0400

    Deployment initiated

--- dj_ai_employee_main/settings.py
+++ dj_ai_employee_main/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =config('DEBUG', default=False, cast=bool)
 
+# you do not use exact this in prod
 ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
 
